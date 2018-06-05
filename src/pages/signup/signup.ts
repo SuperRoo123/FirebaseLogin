d613f0b94a670ddc3dea4d02cc998e5aa562047d
SuperRoo123 <rgr.burke@gmail.com>
2018-06-05 17:35:17 +1000

    Add files via upload

--- src/pages/signup/signup.ts
+++ src/pages/signup/signup.ts
@@ -1,63 +1,71 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 import {
-  IonicPage, NavController, NavParams, Loading,
+  Alert,
+  AlertController,
+  IonicPage,
+  Loading,
   LoadingController,
-  AlertController} from 'ionic-angular';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { AuthProvider } from '../../providers/auth/auth';
-import { EmailValidator } from '../../validators/email';
-import { HomePage } from '../home/home';
+  NavController
+} from "ionic-angular";
+import { FormBuilder, FormGroup, Validators } from "@angular/forms";
+import { AuthProvider } from "../../providers/auth/auth";
+import { EmailValidator } from "../../validators/email";
+import { HomePage } from "../home/home";
 
-@IonicPage({ name: 'signup'})
+@IonicPage()
 @Component({
-  selector: 'page-signup',
-  templateUrl: 'signup.html',
+  selector: "page-signup",
+  templateUrl: "signup.html"
 })
 export class SignupPage {
   public signupForm: FormGroup;
   public loading: Loading;
-  constructor(public navCtrl: NavController, public navParams: NavParams, public authProvider: AuthProvider, public formBuilder: FormBuilder,
-    public loadingCtrl: LoadingController, public alertCtrl:
-      AlertController) {
-    
-    this.signupForm = formBuilder.group({
-      email: ['', Validators.compose([Validators.required,
-      EmailValidator.isValid])],
-      password: ['', Validators.compose([Validators.minLength(6),
-      Validators.required])]
-    });
 
+  constructor(
+    public navCtrl: NavController,
+    public authProvider: AuthProvider,
+    public loadingCtrl: LoadingController,
+    public alertCtrl: AlertController,
+    formBuilder: FormBuilder
+  ) {
+    this.signupForm = formBuilder.group({
+      email: [
+        "",
+        Validators.compose([Validators.required, EmailValidator.isValid])
+      ],
+      password: [
+        "",
+        Validators.compose([Validators.minLength(6), Validators.required])
+      ]
+    });
   }
-  signupUser() {
+  signupUser(): void {
     if (!this.signupForm.valid) {
-      console.log(this.signupForm.value);
+      console.log(
+        `Need to complete the form, current value: ${this.signupForm.value}`
+      );
     } else {
-      this.authProvider.signupUser(this.signupForm.value.email,
-        this.signupForm.value.password)
-        .then(() => {
+      const email: string = this.signupForm.value.email;
+      const password: string = this.signupForm.value.password;
+
+      this.authProvider.signupUser(email, password).then(
+        user => {
           this.loading.dismiss().then(() => {
             this.navCtrl.setRoot(HomePage);
           });
-        }, (error) => {
+        },
+        error => {
           this.loading.dismiss().then(() => {
-            let alert = this.alertCtrl.create({
+            const alert: Alert = this.alertCtrl.create({
               message: error.message,
-              buttons: [
-                {
-                  text: "Ok",
-                  role: 'cancel'
-                }
-              ]
+              buttons: [{ text: "Ok", role: "cancel" }]
             });
             alert.present();
           });
-        });
+        }
+      );
       this.loading = this.loadingCtrl.create();
       this.loading.present();
     }
   }
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad SignupPage');
-  }
-
 }
