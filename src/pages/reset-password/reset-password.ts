4b4fee30acb7c4fa3fb52e5bdbe892c6f3708f2e
SuperRoo123 <rgr.burke@gmail.com>
2018-06-05 17:34:43 +1000

    Add files via upload

--- src/pages/reset-password/reset-password.ts
+++ src/pages/reset-password/reset-password.ts
@@ -1,59 +1,66 @@
-import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams, AlertController } from 'ionic-angular';
-import { FormBuilder, Validators, FormGroup } from '@angular/forms';
-import { AuthProvider } from '../../providers/auth/auth';
-import { EmailValidator } from '../../validators/email';
+import { Component } from "@angular/core";
+import {
+  Alert,
+  AlertController,
+  IonicPage,
+  NavController
+} from "ionic-angular";
+import { FormBuilder, FormGroup, Validators } from "@angular/forms";
+import { AuthProvider } from "../../providers/auth/auth";
+import { EmailValidator } from "../../validators/email";
 
-@IonicPage({
-  name: 'reset-password'
-})
+@IonicPage()
 @Component({
-  selector: 'page-reset-password',
-  templateUrl: 'reset-password.html',
+  selector: "page-reset-password",
+  templateUrl: "reset-password.html"
 })
 export class ResetPasswordPage {
-
   public resetPasswordForm: FormGroup;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams,
-  public authProvider: AuthProvider, public formBuilder: FormBuilder,
-    public alertCtrl: AlertController) {
-    
+  constructor(
+    public navCtrl: NavController,
+    public authProvider: AuthProvider,
+    public alertCtrl: AlertController,
+    formBuilder: FormBuilder
+  ) {
     this.resetPasswordForm = formBuilder.group({
-      email: ['', Validators.compose([Validators.required,
-      EmailValidator.isValid])],
+      email: [
+        "",
+        Validators.compose([Validators.required, EmailValidator.isValid])
+      ]
     });
-
   }
-  resetPassword() {
+  resetPassword(): void {
     if (!this.resetPasswordForm.valid) {
-      console.log(this.resetPasswordForm.value);
+      console.log(
+        `Form is not valid yet, current value: ${this.resetPasswordForm.value}`
+      );
     } else {
-      this.authProvider.resetPassword(this.resetPasswordForm.value.email)
-        .then((user) => {
-          let alert = this.alertCtrl.create({
-            message: "We just sent you a reset link to your email",
+      const email: string = this.resetPasswordForm.value.email;
+      this.authProvider.resetPassword(email).then(
+        user => {
+          const alert: Alert = this.alertCtrl.create({
+            message: "Check your email for a password reset link",
             buttons: [
               {
                 text: "Ok",
-                role: 'cancel',
-                handler: () => { this.navCtrl.pop(); }
+                role: "cancel",
+                handler: () => {
+                  this.navCtrl.pop();
+                }
               }
             ]
           });
           alert.present();
-        }, (error) => {
-          var errorMessage: string = error.message;
-          let errorAlert = this.alertCtrl.create({
-            message: errorMessage,
-            buttons: [{ text: "Ok", role: 'cancel' }]
+        },
+        error => {
+          const errorAlert = this.alertCtrl.create({
+            message: error.message,
+            buttons: [{ text: "Ok", role: "cancel" }]
           });
           errorAlert.present();
-        });
+        }
+      );
     }
   }
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad ResetPasswordPage');
-  }
-
 }
